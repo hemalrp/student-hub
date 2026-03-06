192db77d2a8239ba486f6ff8a7d53d3b6e71fa1e
hemalrp <141286887+hemalrp@users.noreply.github.com>
2026-03-05 23:56:03 -0800

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://djfnwqeqldfszzjizwino.supabase.co",
-  "sb_publishable_9mdpDSMn9Ultz3jFLYxAJw_v9Pdup9q"
-);
